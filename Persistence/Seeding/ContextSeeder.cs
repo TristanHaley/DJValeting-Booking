276c0587f0ce526de7dcabac47cd49b4fbe4bf9c
Tristan Haley <th185132@ncr.com>
2021-08-17 11:04:14 +0100

    ** Work Paused ** Adding database seeders.

---
+++ Persistence/Seeding/ContextSeeder.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Application.Interfaces;
+
+namespace Persistence.Seeding
+{
+    public abstract class ContextSeeder
+    {
+        public abstract Task<bool> SeedDatabase(IDjValetingContext context);
+    }
+}
---
+++ Persistence/Seeding/CustomerSeeder.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Application.Interfaces;
+using Bogus;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Persistence.Seeding
+{
+    public class CustomerSeeder : ContextSeeder
+    {
+        private Faker<Customer> CustomerFaker =>
+            new Faker<Customer>()
+               .Ignore(customer => customer.Bookings)
+               .RuleFor(customer => customer.Firstname, faker => faker.Person.FirstName)
+               .RuleFor(customer => customer.Surname, faker=>faker.Person.LastName)
+               .RuleFor(customer => customer.ContactEmail, faker=>faker.Person.Email)
+               .RuleFor(customer => customer.ContactNumber, faker => faker.Person.Phone);
+        
+        public override async Task<bool> SeedDatabase(IDjValetingContext context)
+        {
+            // Warn: Work paused
+            using var transaction = await context.BeginTransactionAsync();
+
+            return false;
+        }
+    }
+}
