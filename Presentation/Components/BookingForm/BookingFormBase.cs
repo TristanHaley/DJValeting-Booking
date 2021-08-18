68279e818e946400115114f1719ad9d4bf43d16b
Tristan Haley <th185132@ncr.com>
2021-08-18 11:53:35 +0100

    ** Work Commenced : 11:00 ** Added booking form UI

--- Presentation/Components/BookingForm/BookingFormBase.cs
+++ Presentation/Components/BookingForm/BookingFormBase.cs
@@ -1,15 +1,20 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using System;
+using Microsoft.AspNetCore.Components;
 using MudBlazor;
 
 namespace Presentation.Components.BookingForm
 {
     public class BookingFormBase : ComponentBase
     {
-        protected string   Firstname   { get; set; }
-        protected string   Surname     { get; set; }
-        protected int      Flexibility { get; set; }
-        protected MudForm  Form        { get; set; }
-        public    bool     FormValid   { get; set; }
-        public    string[] FormErrors  { get; set; }
+        protected string    Firstname   { get; set; }
+        protected string    Surname     { get; set; }
+        protected int       Flexibility { get; set; }
+        protected string    Email       { get; set; }
+        protected string    Number      { get; set; }
+        protected MudForm   Form        { get; set; }
+        public    bool      FormValid   { get; set; }
+        public    string[]  FormErrors  { get; set; }
+        public    DateTime? When        { get; set; }
+        public    int       VehicleType { get; set; }
     }
 }
